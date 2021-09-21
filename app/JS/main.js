f5eed84ec8204fa65f2d4776a980f41f6d28cf24
Egor-Almurzin <egor-almurzin@rambler.ru>
2021-09-21 21:42:44 +0400

    added lesson 12

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,3 +1,23 @@
 $(function(){
-  
+
+  $('.catalog').on('click', function(){
+    $('.catalog__list').slideToggle();
+  });
+
+  $('.catalog__list a').on('click', function(){
+    $('.catalog__list').slideUp('catalog');    
+  });
+
+  // $('.header').not('.catalog, .catalog__list a').on('click', function(){
+  //   $('.catalog__list').slideUp('catalog');    
+  // });
+
+  $('.best-offer').slick({
+    arrows: true,
+    dots: false,
+    // autoplay: true,
+    // autoplaySpeed: 4000,
+  });
+
+    var mixer = mixitup('.products');
 });
